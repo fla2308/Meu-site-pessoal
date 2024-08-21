dfd629cebf52844114b5dc759d5618270bed2fa2
fla2308 <Flaviasilvamaia123@gmail.com>
2024-08-21 14:47:40 -0300

    atualizando site

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import api from "./services/api";
+
 import './App.css';
 
 import BarraNavegacao from "./componentes/BarraNavegacao/BarraNavegacao";
@@ -13,9 +15,42 @@ import Contato from "./componentes/Contato/Contato";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+   const [informacoes,setInformacoes] = React.useState({});
+   const [curriculo,setCurriculo] = React.useState({});
+   const [portfolio,setPortfolio] = React.useState({});
+
+   const fetchDados = async () => {
+    try{ 
+      const informacao = await api.get ('/informacoes/1');
+      setInformacoes ({
+        foto: informacao.data.foto,
+        nome: informacao.data.nome,
+        cargo: informacao.data.cargo, 
+      });
+      const experienciaacademica = await api.get ('/experiencias?tipo=academica');
+      const experienciaprofissional = await api.get ('/experiencias?tipo=profissional');
+
+
+      setCurriculo ({
+        resumo: informacao.data.resumo,
+        experienciaAcademica:experienciaacademica.data,
+        experienciaProfissional:experienciaprofissional.data
+      });
+
+      const portfolio = await api.get ('/portfolio');
+      setPortfolio (portfolio.data);
+
+    } catch (error) {
+      console.error ('Erro ao buscar dados:',error);
+    }
+  };
+  React.useEffect(() => {
+    fetchDados();
+  }, []);
+
   return (
 <>
-<Cabecalho></Cabecalho>
+<Cabecalho informacoes={informacoes}></Cabecalho>
 
 <BrowserRouter>
 <BarraNavegacao></BarraNavegacao>
